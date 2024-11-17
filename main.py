838aab6aa4b91d2d0c3b8a8ccc1bc794c77ba71f
Jyotin Goel <b22ai063@iitj.ac.in>
2024-11-17 18:46:44 +0530

    PIPELINE COMPLETE - ORCHESTRATOR
    Signed-off-by: Jyotin Goel <b22ai063@iitj.ac.in>

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from Airflow.orchestrator import pipeline_populate_db
 
 if __name__ == "__main__":
     pipeline_populate_db(
-        url="https://en.wikipedia.org/wiki/Alan_Turing",
-        name_dataset="demo_viva"
+        url="https://pastebin.com/raw/N3HCnu7m",
+        name_dataset="demo_viva_1"
     )
